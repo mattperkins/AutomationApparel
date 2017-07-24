d993168618584c0cf6e0e0272da293a2064286ad
mattperkins <mattperkins@users.noreply.github.com>
2017-07-24 22:38:46 +0100

    cl

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import style from 'styled-components'
+import './video.css'
 
-class App extends Component {
+const Wrapper = style.div`
+	max-width: 1200px;
+`
+const Video = style.div`
+position: fixed;
+height: 100%;
+width: 100%;
+top: 0;
+left: 0;
+`
+export default class App extends Component {
+	constructor(props) {
+	super(props)
+	this.state = {
+	}
+}
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+	<Wrapper>
+
+<Video>		
+	
+	<iframe height="100%" width="100%" src="https://www.youtube.com/embed/i9IkIl3U02A?&autoplay=1&mute=1" frameborder="0" title="Automation Apparel" allowfullscreen/>
+  		
+
+</Video>		
+	</Wrapper>
     );
   }
 }
 
-export default App;
